5fdabd3b6b7877deea3610a93e007f5f4bbc057c
Raphaël Haltz <lepointgeek@gmail.com>
2017-01-20 16:47:42 +0100

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        Point p3 = new Point(0, 0);
+        Point p4 = new Point(0, 0);
 
-        Trapeze trapeze = new Trapeze();
+        Trapeze trapeze = new Trapeze(p1, p2, p3, p4);
+
+        
     }
 }
