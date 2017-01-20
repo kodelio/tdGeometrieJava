24136baebba410c43551f11052da2889e3aa5261
Raphaël Haltz <lepointgeek@gmail.com>
2017-01-20 16:46:23 +0100

    Updater Parameters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import Geometrie.Point;
+import Geometrie.Polygone.Trapeze;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Point p1 = new Point(0, 0);
+
+        Trapeze trapeze = new Trapeze();
     }
 }
